d4aeb27b34051001e36c080fa8bb7bbac78f55d4
Jacob Garcia <jacobgarcia@aggienetwork.com>
2019-06-03 22:39:24 -0500

    text

--- src/components/About.js
+++ src/components/About.js
@@ -11,9 +11,7 @@ export default ()=> (
     <div className="page-content">
       <div className="section">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p><p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+          "My first job doing pest control was just a summer job, but since then it has sort of stuck with me. I enjoy meeting with people each day and helping to solve their pest control needs. I feel like this is a job that really plays to my strengths and interests because even as a child I was interested in entomology and zoology, and I love how this is a job that allows me to travel and interact with people daily."
         </p>
       </div>
       <Link to="/reviews" className="btn">Reviews</Link>
