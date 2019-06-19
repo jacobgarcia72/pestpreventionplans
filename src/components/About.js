d7ccc7453944cf5a55dbfbd919f79d2f8b132e69
Jacob Garcia <jacobgarcia@aggienetwork.com>
2019-06-19 05:12:32 -0500

    about page image

--- src/components/About.js
+++ src/components/About.js
@@ -14,6 +14,7 @@ export default ()=> (
           "I chose to work in this industry because I love working with clients and solving their pest control issues. I have had an interest in entomology, zoology, and biology since I was a child and I love meeting new people. I enjoy working in a field that allows me to use my knowledge and talents on a daily basis to help homes and businesses to be pest free."
         </p>
         <p>Brandon Garcia, Owner</p>
+        <img alt="Brandon Garcia, Pest Prevention Plans" src="assets/brandon.jpg" />
       </div>
       <Link to="/reviews" className="btn">Reviews</Link>
     </div>
