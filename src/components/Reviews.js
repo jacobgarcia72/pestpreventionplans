47969dd2ac69ba1d14fd94c78c3e1f3ac1c105b5
Jacob Garcia <jacobgarcia@aggienetwork.com>
2019-06-20 19:36:07 -0500

    review page style tweaks

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -86,15 +86,15 @@ export default class Reviews extends React.Component {
               <div id="slide-0" className="slide">
                 <div className="slide-content" id="home-adviser">
 
-                  <h1>Home Advisor</h1>
+                  <a href="https://www.homeadvisor.com/rated.PestPreventionPlansLLC.79551165.html"><h1>Home Advisor</h1></a>
 
                   <div className="row awards">
 
-                    <a rel="noopener noreferrer" target="_blank" href="http://www.homeadvisor.com/c.Pest-Control.Austin.TX.-12057.html#spid=79551165" style={{display: "block", margin: "0 auto"}}><img alt="Top Rated Business by HomeAdvisor" style={{display:"block"}} src={"http://www.homeadvisor.com/images/sp-badges/toprated-solid-border.png?sp=79551165&key=b299f2f981ce0adfe41e33d5b381bb9e"} /></a>
+                    <a rel="noopener noreferrer" target="_blank" href="https://www.homeadvisor.com/c.Pest-Control.Austin.TX.-12057.html#spid=79551165" style={{display: "block", margin: "0 auto"}}><img alt="Top Rated Business by HomeAdvisor" style={{display:"block"}} src={"https://www.homeadvisor.com/images/sp-badges/toprated-solid-border.png?sp=79551165&key=b299f2f981ce0adfe41e33d5b381bb9e"} /></a>
                     
-                    <a rel="noopener noreferrer" target="_blank" href="http://www.homeadvisor.com/c.Pest-Control.Austin.TX.-12057.html#spid=79551165" style={{display: "block", margin: "0 auto"}}><img alt="HomeAdvisor Elite Service Pro - Pest Prevention Plans, LLC" style={{display:"block"}} src={"http://www.homeadvisor.com/images/sp-badges/elite-solid-border.png?sp=79551165&key=b299f2f981ce0adfe41e33d5b381bb9e"} /></a>
+                    <a rel="noopener noreferrer" target="_blank" href="https://www.homeadvisor.com/c.Pest-Control.Austin.TX.-12057.html#spid=79551165" style={{display: "block", margin: "0 auto"}}><img alt="HomeAdvisor Elite Service Pro - Pest Prevention Plans, LLC" style={{display:"block"}} src={"https://www.homeadvisor.com/images/sp-badges/elite-solid-border.png?sp=79551165&key=b299f2f981ce0adfe41e33d5b381bb9e"} /></a>
 
-                    <a  rel="noopener noreferrer" target="_blank" href="http://www.homeadvisor.com/rated.PestPreventionPlansLLC.79551165.html" style={{display: "block", margin: "0 auto"}}><img alt={"Pest Prevention Plans, LLC is a Screened & Approved HomeAdvisor Pro"} style={{display:"block"}} src={"http://www.homeadvisor.com/images/sp-badges/soap-solid-border.png?sp=79551165&key=b299f2f981ce0adfe41e33d5b381bb9e"} /></a>
+                    <a  rel="noopener noreferrer" target="_blank" href="https://www.homeadvisor.com/rated.PestPreventionPlansLLC.79551165.html" style={{display: "block", margin: "0 auto"}}><img alt={"Pest Prevention Plans, LLC is a Screened & Approved HomeAdvisor Pro"} style={{display:"block"}} src={"http://www.homeadvisor.com/images/sp-badges/soap-solid-border.png?sp=79551165&key=b299f2f981ce0adfe41e33d5b381bb9e"} /></a>
 
                   </div>
 
@@ -115,7 +115,7 @@ export default class Reviews extends React.Component {
               <div id="slide-1" className="slide">
                 <div className="slide-content" id="facebook">
 
-                  <h1>Facebook</h1>
+                  <a href="https://www.facebook.com/pestpreventionplans/"><h1>Facebook</h1></a>
                   
                   <div className="facebook-link connect row">
                     <a id="facebook" href="https://www.facebook.com/pestpreventionplans/"  rel="noopener noreferrer" target="_blank">
@@ -136,15 +136,16 @@ export default class Reviews extends React.Component {
               <div id="slide-2" className="slide">
                 <div className="slide-content" id="yelp">
 
-                  <h1>Yelp</h1> 
+                  <a href="https://www.yelp.com/biz/pest-prevention-plans-austin"><h1>Yelp</h1></a> 
+
+                  <div className="yelp-badge" id="yelp-biz-badge-rrc-6JkeIyQ2CbCXluFyyQXuIg"><a  rel="noopener noreferrer" target="_blank" href="http://yelp.com/biz/pest-prevention-plans-austin?utm_medium=badge_star_rating_reviews&amp;utm_source=biz_review_badge">Check out Pest Prevention Plans on Yelp</a></div>
                   
-                  <p>"Very fast response time- was able to communicate back and forth through the Yelp messaging app. After I put in a request for more information - the owner, Brandon was in contact almost immediately and was available to come by our house and check out the situation SAME DAY! Then he scheduled the actual service the next day! Super handy when it comes to getting rid of Pests that no body wants around!</p>
+                  <p id="first-p">"Very fast response time<span className="hide-text">- was able to communicate back and forth through the Yelp messaging app</span>.<span className="hide-text"> After I put in a request for more information - the owner,</span> Brandon was in contact almost immediately and was available to come by our house and check out the situation SAME DAY! <span className="hide-more-text">Then he scheduled the actual service the next day! Super handy when it comes to getting rid of Pests that no body wants around!</span></p>
 
                   <p>"In half a week I've already seen the difference and we are super appreciative about that! Will keep up with maintenance and continue to go with Pest Prevention Plans! Reasonable / Competitive pricing."</p>
 
                   <div className="reviewer">Jenna H.</div>
 
-                  <div className="yelp-badge" id="yelp-biz-badge-rrc-6JkeIyQ2CbCXluFyyQXuIg"><a  rel="noopener noreferrer" target="_blank" href="http://yelp.com/biz/pest-prevention-plans-austin?utm_medium=badge_star_rating_reviews&amp;utm_source=biz_review_badge">Check out Pest Prevention Plans on Yelp</a></div>
 
                 </div>
               </div>
